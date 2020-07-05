e528de592e53495a3e9e74a7396a0dcc51b8c161
Nicodemos234 <nicodemos54324@gmail.com>
2020-07-05 17:07:17 -0300

    Start

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      <ul class="nav">
+        <li class="nav-item">
+          <nuxt-link class="nav-link" to="/">Home</nuxt-link>
+        </li>
+		<li class="nav-item">
+			<nuxt-link class="nav-link" to="/conversor">Conversor</nuxt-link>
+		</li>
+      </ul>
+    </div>
+    <nuxt />
+  </div>
+</template>
+<style>
+@import "../assets/css/reset.css";
+@import "../assets/css/bootstrap.min.css";
+@import "../assets/css/style.css";
+</style>
